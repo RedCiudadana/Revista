fa8b4f24c8f0af79099e4ddb4cd677df57f164a4
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-12-19 15:02:02 -0600

    base cms

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const rmj = require('render-markdown-js')
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.setTemplateFormats("njk,html,md");
+    
+    eleventyConfig.addPassthroughCopy('src');
+    eleventyConfig.addPassthroughCopy('admin');
+
+    eleventyConfig.addNunjucksFilter("rmj", function(content) {
+        return rmj(content);
+    });
+
+    eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
+        return array.slice(0, limit);
+    });
+}
